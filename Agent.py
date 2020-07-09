a0a13c968e1b9c74d20a233a39dde18c822851f8
Gricci95RR <57287930+Gricci95RR@users.noreply.github.com>
2020-07-09 15:49:20 +0200

    Aggiungo metodi in Granule

--- Agent.py
+++ Agent.py
@@ -3,11 +3,12 @@ from Extractor import Extractor
 from Granule import Granule
 
 class Agent:
-    Symbol_Threshold = 0;
-    Labda = 0;
-    ClusteringParams = 0;
+    Symbol_Threshold = 0; 
+    Labda = 0; #max num of clusters
+    ClusteringParams_ID = 0; #list of clusters' pattern IDs
     MetricParams = 0;
-    Representatives = 0;
+    Representatives = 0; #list of clusters' medoids
+    
     def __init__(self,name,nome_colonna,theta, Q,dissimilarityFunction):
         self.Symbol_Threshold = theta;
         self.Labda = Q; #__verifica__
@@ -15,15 +16,20 @@ class Agent:
         data_frame = e.Extract(name)
         colonna = data_frame[nome_colonna].tolist()
         g = Granulator(theta, Q); #dichiaro oggetto di tipo Granulator
-        self.ClusteringParams, self.Representatives = g.Process(colonna, theta, Q,dissimilarityFunction)
+        self.ClusteringParams_ID, self.Representatives = g.Process(colonna, theta, Q,dissimilarityFunction)
+        
     def get_Symbol_Threshold(self):
         return self.Symbol_Threshold
+    
     def get_Labda(self):
         return self.Labda
-    def get_ClusteringParams(self):
-        return self.ClusteringParams
+    
+    def get_ClusteringParams_ID(self):
+        return self.ClusteringParams_ID
+    
     def get_MetricParams(self):
         return self.MetricParams
+    
     #def Extract(self, name):
     #    e = Extractor(); #dichiaro oggetto di tipo Extractor
     #    data_frame = e.Extract('iris_data.txt')
--- Granule.py
+++ Granule.py
@@ -6,5 +6,24 @@ class Granule:
     Representative = 0;
     Metric = 0;
     
+    def set_Compactness(self, Compactness):
+        self.Compactness = Compactness
+        
+    def set_Cardinality(self, Cardinality):
+        self.Cardinality = Cardinality
+    
+    def set_Effective_Radius(self, Effective_Radius):
+        self.Effective_Radius = Effective_Radius
+    
+    def set_Quality(self, Quality):
+        self.Quality = Quality
+    
+    def set_Representative(self, Representative):
+        self.Representative = Representative
+    
+    def set_Metric(self, Metric):
+        self.Metric = Metric
+        
+        
 
 
