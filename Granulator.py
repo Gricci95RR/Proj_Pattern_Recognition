44631a22e64cf3f422b719223b1a77b15c19e136
Gricci95RR <57287930+Gricci95RR@users.noreply.github.com>
2020-09-10 13:08:27 +0200

    Elimino Metodo Process1

--- Granulator.py
+++ Granulator.py
@@ -26,34 +26,4 @@ class Granulator:
     def Process(self, dataset, obj_clustering_bsas):  # SpareBSAS
         self.obj_clustering_bsas.clustering_bsas(dataset,self.Lambda,self.Symbol_Threshold,self.obj_metric,self.obj_representative)
    
-    def Process1(self,sample,diss):
-        cardinalita = [];
-        self.sample = sample;
-        bsas_instance = bsas(self.sample, self.Lambda, self.Symbol_Threshold,diss)
-        bsas_instance.process()
-        # Get clustering results.
-        clusters = bsas_instance.get_clusters()
-        representatives = bsas_instance.get_representatives()
-        bsas_visualizer.show_clusters(self.sample, clusters, representatives)
-        
-        # Calcolo cardinalità
-        for i in range(0,len(clusters)):
-            cardinalita.append(len(clusters[i]))
-         
-        # Creazione oggetto granulo
-        granulo = Granule()
-        # Set Params oggetto granulo
-        granulo.set_Representative(representatives)
-        granulo.set_Cardinality(cardinalita)
-        # Inserimento in lista oggetto granulo
-        self.lista_di_granuli.append(granulo)
-        
-        print('lista di granuli:')
-        print(self.lista_di_granuli)
-        print('cardinalità:')  
-        print(cardinalita)
-        print('clusters:')
-        print(clusters)
-        print('representatives:')
-        print(representatives)
-        return clusters, representatives
+
